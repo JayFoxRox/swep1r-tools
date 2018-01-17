9da8e527e1b02b15bacb00432c63ff501758856d
Unknown <rienafairefr@gmail.com>
2018-01-17 21:14:31 +0100

    help instead of description, duh

--- arguments.py
+++ arguments.py
@@ -1,8 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser('SWE1R scripts ')
-parser.add_argument('--out', description='directory where the output files are written', default='/tmp/swep1r')
-parser.add_argument('input', description='input file')
+parser.add_argument('--out', help='directory where the output files are written', default='/tmp/swep1r')
+parser.add_argument('input', help='input file')
 
 multiparser = argparse.ArgumentParser('SWE1R scripts ')
-multiparser.add_argument('input', nargs='+', description='input files')
+multiparser.add_argument('input', nargs='+', help='input files')
